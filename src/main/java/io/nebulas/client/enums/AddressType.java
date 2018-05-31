2103b4b278ba64bebb574cae7681ca54a57b5e4f
liuzhangjie <33512636+liuzhangjie@users.noreply.github.com>
2018-05-31 20:25:14 +0800

    Update AddressType.java

--- src/main/java/io/nebulas/client/enums/AddressType.java
+++ src/main/java/io/nebulas/client/enums/AddressType.java
@@ -5,18 +5,31 @@ package io.nebulas.client.enums;
  **/
 public enum AddressType {
 
+    
     NORMAL_ADDRESS(87) , CONTRACT_ADDRESS(88);
 
     private Integer value;
 
+    /**
+    * AddressType constructor
+    *
+    * @parameter {Integer} value
+    *
+    */
     AddressType(Integer value) {
         this.value = value;
     }
 
+    /**
+    * method get the value
+    */
     public Integer getValue() {
         return value;
     }
 
+    /**
+    * method set the value and get the object
+    */
     public AddressType setValue(Integer value) {
         this.value = value;
         return this;
