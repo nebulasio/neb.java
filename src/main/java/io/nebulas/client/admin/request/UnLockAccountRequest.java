d01a5dd413f07bb0e0641f8541f9283e0269fbfa
liuzhangjie <33512636+liuzhangjie@users.noreply.github.com>
2018-05-31 21:43:54 +0800

    Update UnLockAccountRequest.java

--- src/main/java/io/nebulas/client/admin/request/UnLockAccountRequest.java
+++ src/main/java/io/nebulas/client/admin/request/UnLockAccountRequest.java
@@ -6,13 +6,20 @@ import java.io.Serializable;
  * Create by liuyang89 on 2018/05/10
  **/
 public class UnLockAccountRequest implements Serializable {
-
+    /**
+    * @property {String} address
+    * @property {String} passphrase
+    * @property {String} duration
+    */
     private String address;
 
     private String passphrase;
 
     private String duration;
 
+    /**
+    * UnLockAccountRequest constructor
+    */
     public UnLockAccountRequest() {
     }
 
@@ -22,28 +29,46 @@ public class UnLockAccountRequest implements Serializable {
         this.duration = duration;
     }
 
+    /**
+    * method get the address
+    */
     public String getAddress() {
         return address;
     }
 
+    /**
+    * method set the address and get the object
+    */
     public UnLockAccountRequest setAddress(String address) {
         this.address = address;
         return this;
     }
 
+    /**
+    * method get the passphrase
+    */
     public String getPassphrase() {
         return passphrase;
     }
 
+    /**
+    * method set the passphraser and get the object
+    */
     public UnLockAccountRequest setPassphrase(String passphrase) {
         this.passphrase = passphrase;
         return this;
     }
 
+    /**
+    * method get the duration
+    */
     public String getDuration() {
         return duration;
     }
 
+    /**
+    * method set the duration and get the object
+    */
     public UnLockAccountRequest setDuration(String duration) {
         this.duration = duration;
         return this;
