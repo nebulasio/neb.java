b71c2f7db00b67ead908cd5c6cfee398221917a5
liuzhangjie <33512636+liuzhangjie@users.noreply.github.com>
2018-05-31 21:12:47 +0800

    Update SignHashRequest.java

--- src/main/java/io/nebulas/client/admin/request/SignHashRequest.java
+++ src/main/java/io/nebulas/client/admin/request/SignHashRequest.java
@@ -6,13 +6,20 @@ import java.io.Serializable;
  * Create by liuyang89 on 2018/05/10
  **/
 public class SignHashRequest implements Serializable {
-
+    /**
+    * @property {String} address
+    * @property {String} hash
+    * @property {Integer} alg
+    */
     private String address;
 
     private String hash;
 
     private Integer alg;
 
+    /**
+    * SignHashRequest constructor
+    */
     public SignHashRequest() {
     }
 
@@ -22,28 +29,46 @@ public class SignHashRequest implements Serializable {
         this.alg = alg;
     }
 
+    /**
+    * method get the address
+    */
     public String getAddress() {
         return address;
     }
 
+    /**
+    * method set the address and get the object
+    */
     public SignHashRequest setAddress(String address) {
         this.address = address;
         return this;
     }
 
+    /**
+    * method get the hash 
+    */
     public String getHash() {
         return hash;
     }
 
+    /**
+    * method set the hash and get the object
+    */
     public SignHashRequest setHash(String hash) {
         this.hash = hash;
         return this;
     }
 
+    /**
+    * method get the alg
+    */
     public Integer getAlg() {
         return alg;
     }
 
+    /**
+    * method set the alg and get the object
+    */
     public SignHashRequest setAlg(Integer alg) {
         this.alg = alg;
         return this;
