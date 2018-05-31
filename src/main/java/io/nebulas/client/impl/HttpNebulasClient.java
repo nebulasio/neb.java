39f39d8a66e23b885dc4e1ea0c20d661b05a888d
liuzhangjie <33512636+liuzhangjie@users.noreply.github.com>
2018-05-31 18:09:06 +0800

    Update HttpNebulasClient.java

--- src/main/java/io/nebulas/client/impl/HttpNebulasClient.java
+++ src/main/java/io/nebulas/client/impl/HttpNebulasClient.java
@@ -17,6 +17,9 @@ public class HttpNebulasClient implements NebulasClient {
 
     private HttpClient client;
 
+    /**
+    * method get instance of HttpNebulasAdminClient
+    */
     public static NebulasClient create(String host) {
         return new HttpNebulasClient(host);
     }
@@ -25,21 +28,38 @@ public class HttpNebulasClient implements NebulasClient {
         return new HttpNebulasClient(host , okHttpClient);
     }
 
+    /**
+    * HttpNebulasClient constructor with parameter host
+    *
+    * @param {String} host
+    */
     public HttpNebulasClient(String host) {
         this.host = host;
         this.client = new OKHttpClient();
     }
 
+    /**
+    * HttpNebulasClient constructor with parameters host and client
+    *
+    * @param {String} host
+    * @param {OkHttpClient} client - Instance of OkHttpClient
+    */
     public HttpNebulasClient(String host, OkHttpClient client) {
         this.host = host;
         this.client = new OKHttpClient(client);
     }
 
+    /**
+    * method get the neb state
+    */
     @Override
     public Response<NebState> getNebState() {
         return client.get(host + "/v1/user/nebstate", new TypeToken<Response<NebState>>() {});
     }
 
+    /**
+    * method get the state of acount
+    */
     @Override
     public Response<AccountState> getAccountState(GetAccountStateRequest request) {
         return client.post(host + "/v1/user/accountstate", request, new TypeToken<Response<AccountState>>() {});
@@ -50,46 +70,69 @@ public class HttpNebulasClient implements NebulasClient {
         return client.get(host + "/v1/user/lib", new TypeToken<Response<Block>>(){});
     }
 
+    /**
+    * method 
+    */
     @Override
     public Response<CallResult> call(CallRequest request) {
         return client.post(host + "/v1/user/call", request , new TypeToken<Response<CallResult>>(){});
     }
 
+    
     @Override
     public Response<RawTransaction> sendRawTransaction(SendRawTransactionRequest request) {
         return client.post(host + "/v1/user/rawtransaction",request ,  new TypeToken<Response<RawTransaction>>(){});
     }
 
+    /**
+    * method get the block by hash
+    */
     @Override
     public Response<Block> getBlockByHash(GetBlockByHashRequest request) {
         return client.post(host + "/v1/user/getBlockByHash", request , new TypeToken<Response<Block>>(){});
     }
 
+    /**
+    * method get the bolck by heigaht
+    */
     @Override
     public Response<Block> getBlockByHeight(GetBlockByHeightRequest request) {
         return client.post(host + "/v1/user/getBlockByHeight", request , new TypeToken<Response<Block>>(){});
     }
 
+    /**
+    * method get the information of transaction
+    */
     @Override
     public Response<TransactionReceipt> getTransactionReceipt(GetTransactionReceiptRequest request) {
         return client.post(host + "/v1/user/getTransactionReceipt", request , new TypeToken<Response<TransactionReceipt>>(){});
     }
 
+    
     @Override
     public Response<Event> subscribe(SubscribeRequest request) {
         return client.post(host + "/v1/user/subscribe", request , new TypeToken<Response<Event>>(){});
     }
 
+    /**
+    * method get the gas price
+    */
     @Override
     public Response<GasPrice> getGasPrice() {
         return client.get(host + "/v1/user/getGasPrice", new TypeToken<Response<GasPrice>>(){});
     }
 
+    /**
+    * method get the stimateeGas
+    */
     @Override
     public Response<EstimateGas> estimateGas(EstimateGasRequest request) {
-        return client.post(host + "/v1/user/estimateGas", request , new TypeToken<Response<EstimateGas>>(){});
+        return client.post(host + "/v1/user/stimateeGas", request , new TypeToken<Response<EstimateGas>>(){});
     }
 
+    /**
+    * method get the events by hash
+    */
     @Override
     public Response<GetEventsByHashResult> getEventsByHash(GetEventByHashRequest request) {
         return client.post(host + "/v1/user/getEventsByHash" , request , new TypeToken<Response<GetEventsByHashResult>>(){});
